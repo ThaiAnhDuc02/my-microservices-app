4550c3b276d3523c35e0bf190d4c15860925d477
Thái Anh Đức <140801842+ThaiAnhDuc02@users.noreply.github.com>
2026-03-25 10:13:01 +0700

    Update server log message to reflect new identifier

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(3000, () => {
-  console.log('Server running on port 3000, duc-test');
+  console.log('Server running on port 3000, duc-test1');
 });
