cbdc4af6b6b23aca9f14d19e5117036dd4ebe618
Thái Anh Đức <140801842+ThaiAnhDuc02@users.noreply.github.com>
2026-03-25 10:11:10 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(3000, () => {
-  console.log('Server running on port 3000, duc');
+  console.log('Server running on port 3000, duc-test');
 });
