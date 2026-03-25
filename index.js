ca5c27b631d429a445dc239e5493689f7621f5fe
ThaiAnhDuc <thaiducdev@gmail.com>
2026-03-25 10:46:51 +0700

    Test Jenkins auto-build trigger

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello from my-app!\n');
+  res.end('Hello from my-app v2.0 - Testing Jenkins auto-build!\n');
 });
 
 server.listen(3000, () => {
-  console.log('Server running on port 3000, duc-test1');
+  console.log('Server running on port 3000 - Build test');
 });
