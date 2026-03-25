775ef82d9a711d14942cf570f4dcfafb4c44f416
Thái Anh Đức <140801842+ThaiAnhDuc02@users.noreply.github.com>
2026-03-25 12:12:31 +0700

    Update response message to v3.0

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello from my-app v2.0 - Testing Jenkins auto-build!\n');
+  res.end('Hello from my-app v3.0 - Testing Jenkins auto-build!\n');
 });
 
 server.listen(3000, () => {
